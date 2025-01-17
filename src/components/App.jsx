0bf45b97b12b87dfcf71bb29dbc86823da448dae
Frankblation <124920027+Frankblation@users.noreply.github.com>
2025-01-17 17:16:23 -0600

    pushing pretty

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,10 @@ import MusicPlayer from "./MusicPlayer";
 function App() {
   return (
     <div className="h-full flex flex-col justify-between p-8 min-h-screen">
+    
       <MusicPlayer />
       <Footer />
+       
     </div>
   );
 }
